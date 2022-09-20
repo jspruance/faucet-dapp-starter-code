5f81524dfc22c9b302a928e809251b8ce80cc581
Jonathan Spruance <jspruance@inflightcorp.com>
2022-09-19 23:39:17 -0400

    Adding code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <nav className="navbar">
+        <div className="container">
+          <div className="navbar-brand">
+            <h1 className="navbar-item is-size-4">Ocean Token (OCT)</h1>
+          </div>
+          <div id="navbarMenu" className="navbar-menu">
+            <div className="navbar-end is-align-items-center">
+              <button className="button is-white connect-wallet">
+                <span className="is-link has-text-weight-bold">
+                  Connect Wallet
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <section className="hero is-fullheight">
+        <div className="faucet-hero-body">
+          <div className="container has-text-centered main-content">
+            <h1 className="title is-1">Faucet</h1>
+            <p>Fast and reliable. 10 OCT/day.</p>
+            <div className="box address-box">
+              <div className="columns">
+                <div className="column is-four-fifths">
+                  <input
+                    className="input is-medium"
+                    type="text"
+                    placeholder="Enter your wallet address (0x...)"
+                  />
+                </div>
+                <div className="column">
+                  <button className="button is-link is-medium">
+                    GET TOKENS
+                  </button>
+                </div>
+              </div>
+              <article className="panel is-grey-darker">
+                <p className="panel-heading">Transaction Data</p>
+                <div className="panel-block">
+                  <p>transaction data</p>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </section>
+    </body>
   );
 }
 
